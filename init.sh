d77029bb5d8d03a6f0924b06a8e9023492df4cfd
Osama M'hiri <59898800+Ryder05@users.noreply.github.com>
2022-06-23 16:09:29 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,9 +5,9 @@ export OPENCELL_PORT=8
 echo ">>> Downloading opencell softwares & docker images"
 mkdir input-files 
 cd input-files
-wget -L http://dl.opencellsoft.com/current/opencell.war -o opencell.war
-wget -L http://dl.opencellsoft.com/current/import-postgres.sql -o import-postgres.sql
-wget -L http://dl.opencellsoft.com/keycloak/init-user-db.sh -o init-user-db.sh
+wget -L http://dl.opencellsoft.com/current/opencell.war  --output-document opencell.war
+wget -L http://dl.opencellsoft.com/current/import-postgres.sql  --output-document import-postgres.sql
+wget -L http://dl.opencellsoft.com/keycloak/init-user-db.sh  --output-document init-user-db.sh
 echo "elasticsearch.restUri=" > opencell-admin.properties
 chown  1000:1000 opencell-admin.properties
 echo "Pulling docker images from docker hub"
